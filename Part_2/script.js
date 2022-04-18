7f229714ea0d4d42f2ea8eb6c6d3f0d4c9e61baa
TonyB <tonybaromagic@gmail.com>
2022-04-18 01:34:45 -0400

    works but wordy js

--- Part_2/script.js
+++ Part_2/script.js
@@ -1,20 +1,35 @@
 console.log("page loaded...")
 
-var likeCount = [5,34]
+var likeCount = [5,34,5]
 
 function drawLikes(){
-    document.querySelector(".likes p").innerText = likeCount[0]+" Likes"
+    console.log("drawing likes")
+    document.getElementById("likes1").innerText = likeCount[0]+" Likes"
+    document.getElementById("likes2").innerText = likeCount[1]+" Likes"
+    document.getElementById("likes3").innerText = likeCount[2]+" Likes"
 }
 drawLikes()
 
 function buttonPress(element){
     // console.log("liking")
     element.classList.add("pressed")
-    likeCount[0]++
-    document.querySelector(".likes p").innerText = likeCount[0]+" Likes"
+    drawLikes()
 }
 
 function buttonUnpress(element){
     // console.log("liked")
     element.classList.remove("pressed")
+}
+
+function likesUp1(){
+    console.log("liking 1")
+    likeCount[0]++
+
+}
+
+function likesUp2(){
+    likeCount[1]++
+}
+function likesUp3(){
+    likeCount[2]++
 }
